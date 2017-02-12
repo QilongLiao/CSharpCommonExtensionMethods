dbce616356dac48a6779785e0c0cf1334cc06efc
FindingPleased <qilongliao@qq.com>
2017-02-13 00:07:50 +0800

    add string Extension Methods

---
+++ CommonExtensionMethods/CommonExtensionMethods/CommonExtensionMethods_String.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonExtensionMethods.QilongLiao
+{
+    public static class CommonExtensionMethods_String
+    {
+        public static IEnumerable<Byte> ToBytes(this string source, Encoding encoding = null)
+        {
+            if (encoding == null)
+            {
+                encoding = Encoding.Unicode;
+            }
+
+            if (!string.IsNullOrWhiteSpace(source))
+            {
+                return encoding.GetBytes(source);
+            }
+            return new byte[0];
+        }
+
+        public static bool IsNullOrWhiteSpace(this string source)
+        {
+            return string.IsNullOrWhiteSpace(source);
+        }
+        public static string IsNullOrWhiteSpace(this IEnumerable<string> source,)
+        {
+            return string.Join(source);
+        }
+
+    }
+}
